305290a8293d1562bc17bd238224aeaab1ee4e34
Ana Santos <ana.br@hotmail.com>
2023-12-07 16:31:50 -0300

    desenvolvimento do header

--- ceu/components/Header/Header.jsx
+++ ceu/components/Header/Header.jsx
@@ -3,7 +3,10 @@ import { Navbar, Nav, Collapse, Dropdown } from 'bootstrap-4-react';
 import star from '../../src/assets/star.png'
 import star2 from '../../src/assets/star2.png'
 import star3 from '../../src/assets/star3.png'
-
+import plane from '../../src/assets/plane.png'
+import logo from '../../src/assets/logoHeader.png'
+import planet from '../../src/assets/bluePlanet.png'
+import planet2 from '../../src/assets/planet.png'
 import style from './header.module.css'
 
 const Header = () => {
@@ -39,10 +42,24 @@ const Header = () => {
       <img className={style.star3} src={star3} alt='imagem de uma estrela dourada' />
       <img className={style.star3} src={star3} alt='imagem de uma estrela dourada' />
       
-
-
+    
+    </div>
+    <section className={style.headerContent}>
+    <div className={style.foguete}>
+      <h1>Bem-vindo ao Centro Educacional Universo Infantil.<br/>
+      Onde Cada Criança é uma Estrela em Ascensão!</h1>
+      <div className={style.effects}>
+        <img className={style.airplane} src={plane} alt='Foguete com 3 crianças' />
+        <img className={style.plan1} src={planet} alt='planeta' />
+        <img className={style.plan2} src={planet2} alt='planeta' />
+      </div>
+      
     </div>
 
+    <div className={style.logo}>
+      <img className={style.logoImag} src={logo} alt='Logomarca da escola' />
+    </div>
+    </section>
 
   </section>
   );
