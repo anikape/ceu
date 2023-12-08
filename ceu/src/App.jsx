dba399ac7a79bf1c35302e8a2350706c53046af9
Ana Santos <ana.br@hotmail.com>
2023-12-08 10:06:44 -0300

    Build

--- ceu/src/App.jsx
+++ ceu/src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Home from '../pages/Home/index'
 import './App.css'
-import Header from '../components/Header/Header'
+
 
 function App() {
   
 
   return (
-    <>
-
-      <Header />
-      
-    </>
+    <Home />
   )
 }
 
